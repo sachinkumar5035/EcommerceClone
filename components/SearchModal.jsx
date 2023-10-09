d41af8a80d099d8509b646e991b332984ed2232c
sachinkumar <sachinkumar5035@gmail.com>
2023-10-09 20:43:28 +0530

    branch created

--- components/SearchModal.jsx
+++ components/SearchModal.jsx
@@ -1,13 +1,18 @@
-import { View, Text, Platform, StatusBar, SafeAreaView, ScrollView } from 'react-native'
+import { View, Text, Platform, StatusBar, SafeAreaView, ScrollView, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { colors } from '../styles/style'
 import { Searchbar } from 'react-native-paper';
+import { useNavigate } from 'react-router-dom';
 
 
 
 
 
 const SearchModal = ({ searchQuery, setActiveSearch, setSearchQuery, products = [] }) => {
+
+
+   
+
     return (
         <View style={{
             height: "100%",
@@ -25,21 +30,70 @@ const SearchModal = ({ searchQuery, setActiveSearch, setSearchQuery, products = 
                     onChangeText={(query) => setSearchQuery(query)}
                     value={searchQuery}
                     style={{
-                        marginTop:20
-                    }} 
+                        marginTop: 20
+                    }}
                 />
                 <ScrollView>
                     <View style={{
-                        paddingVertical:40,
-                        paddingHorizontal:10
+                        paddingVertical: 40,
+                        paddingHorizontal: 10
                     }}>
-
+                        {
+                            products.map((i) => (
+                                <SearchItem
+                                    key={i._id}
+                                    imgSrc={i.images[0]?.url}
+                                    name={i.name}
+                                    price={i.price}
+                                    handler={()=>
+                                        navigate.navigate("productdetails",{id:i._id})
+                                    }
+                                />
+                            ))
+                        }
                     </View>
                 </ScrollView>
-                
+
             </SafeAreaView>
         </View>
     )
 }
+
+const SearchItem = ({price,name,imgSrc,handler})=>{
+    <TouchableOpacity
+        onPress={handler}
+    >
+        <View
+            style={{
+                padding:20,
+                borderRadius:10,
+                backgroundColor:colors.color2,
+                elevation:5,
+                width:"100%",
+                alignItems:"center",
+                justifyContent:"flex-end",
+                flexDirection:"row",
+                marginVertical:30
+            }}
+        >
+            <Image 
+                source={{
+                    uri:imgSrc
+                }}
+                style={{
+                    height:80,
+                    width:80,
+                    position:"absolute",
+                    resizeMode:"contain"
+                }} 
+            />
+
+            
+        </View>
+
+    </TouchableOpacity>
+}
+
+
 
 export default SearchModal
