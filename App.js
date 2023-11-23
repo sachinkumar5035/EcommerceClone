b86491bd4288a0c4bb2fef7814c6295bcc346d80
sachinkumar <sachinkumar5035@gmail.com>
2023-11-23 12:09:39 +0530

    implementing redux

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Main from './Main.jsx';
-
+import {Provider} from 'react-redux'
+import { store } from './redux/store.js';
 
 export default function App () {
   return (
-    <Main/>
+    <Provider store={store}>
+      <Main/>
+    </Provider>
   );
 }
